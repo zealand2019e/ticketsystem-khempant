d0a7974842f7e1e7a5f0c422155d19c6ef038259
khempant <khempant311@gmail.com>
2019-09-20 12:24:31 +0200

    1st step

--- ClassLibrary/Car.cs
+++ ClassLibrary/Car.cs
@@ -3,9 +3,16 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace ClassLibrary
-{
+{/// <summary>
+/// This is my Car Class
+/// </summary>
     public class Car
     {
+        public double Price()
+        {
+            return 240;
+        }
+
 
     }
 }
---
+++ ClassLibrary/Vehicle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary
+{
+    public abstract class Vehicle
+    {
+        private double _price;
+        private bool _brobizz;
+        private string licensePlate;
+        public DateTime Date { get; set; }
+        public abstract string VehicleType();
+
+        public virtual double Price()
+        {
+        }
+    }
+}
